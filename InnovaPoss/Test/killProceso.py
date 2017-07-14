6137b8a86fc2b039aef72b176f7520a120e087a8
Hugo Medina <alexei38@gmail.com>
2017-07-14 13:51:40 +0200

    no message

--- InnovaPoss/Test/killProceso.py
+++ InnovaPoss/Test/killProceso.py
@@ -10,9 +10,11 @@ print('iniciando matado de proceso')
 
 for i in psutil.pids():
     process = psutil.Process(i)
+    print(process.name())
     if process.name() == '3001' or  process.name() == 'CCM' or  process.name() == 'Sockmon' or process.name() == 'Sockserver':
         print(f'matando proceso : {process.name()}')
         process.kill()
+        process.terminate()
         print('proceso muerto')
    
 
